93b7abccaebc34141fc3f2f146fc2e5b0c9d5c61
pear95 <59073064+pear95@users.noreply.github.com>
2021-12-20 12:21:30 +0100

    Match module update: (#11)
    ••Changes:
    - File Flags:
      • Added new enumerator MatchStatus with match info status items (Start, End, Disconnect...)
    - Folder Interface:
      • Created IMatch interface
    -  Interface IMatch:
      • Added fields (int) Index, (string) ID and (MatchStatus) Status
    - Folder Base:
      • Created Match class
    - Class Match:
      • Added fields (string) ID, (int) Index and (MatchStatus) Status. And added constructors.
    - Folder Operations:
      • Created Match folder
    - Folder Match:
      • Created GetStartEndMatchInfo class
      • Created IsMatchInProgress class
    - Class GetStartEndMatchInfo:
      • Created private fields IDs, Index....
      • Created GetMatches method that calculated and returns List of Match (it contains basic information about start/end game data)
    - Class IsMatchInProgress:
      • Created Check method that returns true if match in game is in progress
    - Class Variables:
      • Added const string MessageIDSeparator
    - Class LogRegex:
      • Added Match Regexs (MatchStart, MatchEnd, MatchDisconnect ....)
    - .gitignore
      • update

---
+++ ConsoleLogReaderCSGO/Base/Interfaces/IMatch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleLogReaderCSGO.Base.Interfaces
+{
+    public interface IMatch
+    {
+        public int Index { get; set; }
+        public string ID { get; set; }
+        public MatchStatus Status { get; set; }
+    }
+}
---
+++ ConsoleLogReaderCSGO/Operations/Match/IsMatchInProgress.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleLogReaderCSGO.Operations.Match
+{
+    /// <summary>
+    /// Class to calculate if match is in progress.
+    /// </summary>
+    internal static class IsMatchInProgress
+    {
+        /// <summary>
+        /// Method to check if game is in progress from provided List of Match
+        /// </summary>
+        /// <param name="matches"></param>
+        /// <returns>
+        /// <br>• bool - if game match is in progress</br>
+        /// <br>• int - Index when match start</br>
+        /// </returns>
+        public static (bool, int) Check(List<Base.Match> matches)
+        {
+            if (matches.Count > 0)
+            {
+                if (matches.Last().Status == MatchStatus.Start)
+                {
+                    return (true, matches.Last().Index);
+                }
+            }
+            return (false, -1);
+        }
+    }
+}
